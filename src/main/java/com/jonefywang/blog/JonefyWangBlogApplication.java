9f996ca08c4a58efb61d94efae9998de95e78578
JonefyWang <wangyuanmimi@163.com>
2022-08-23 21:52:23 +0800

    JonefyWangBlog pom文件更新

---
+++ src/main/java/com/jonefywang/blog/JonefyWangBlogApplication.java
@@ -0,0 +1,13 @@
+package com.jonefywang.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JonefyWangBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JonefyWangBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonefywang/blog/JonefyWangBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonefywang.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JonefyWangBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
