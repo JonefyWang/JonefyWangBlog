af92df106a8cc66857f1e50ba71ad1df6ee999d4
JonefyWang <wangyuanmimi@163.com>
2022-08-24 22:47:27 +0800

    上传统一结果集代码

---
+++ src/main/java/com/jonefywang/blog/common/lang/Result.java
@@ -0,0 +1,79 @@
+package com.jonefywang.blog.common.lang;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.sql.SQLType;
+
+/**
+ * @ClassName Result
+ * @Description 统一异常结果
+ * @Author 19285
+ * @Date 2022/8/24 22:20
+ * @Version 1.0
+ */
+@Data
+public class Result implements Serializable {
+    private int code;
+    private String msg;
+    private Object data;
+
+    /**
+     * 成功信息结果集一次封装
+     * @param data
+     * @return
+     */
+    public static Result success( Object data) {
+        return success(200,"操作成功",data);
+
+    }
+
+    /**
+     * 成功信息结果集
+     * @param code
+     * @param msg
+     * @param data
+     * @return
+     */
+    public static Result success(int code, String msg, Object data) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+
+    /**
+     * 失败消息结果集二次封装
+     * @param msg
+     * @return
+     */
+    public static Result fail(String msg) {
+        return fail(400, msg, null);
+    }
+
+    /**
+     * 失败消息结果集一次封装
+     * @param msg
+     * @param data
+     * @return
+     */
+    public static Result fail(String msg, Object data) {
+        return fail(400, msg, data);
+    }
+
+    /**
+     * 失败消息结果集
+     * @param code
+     * @param msg
+     * @param data
+     * @return
+     */
+    public static Result fail(int code, String msg, Object data) {
+        Result result = new Result();
+        result.setCode(code);
+        result.setMsg(msg);
+        result.setData(data);
+        return result;
+    }
+}
--- src/main/java/com/jonefywang/blog/controller/UserController.java
+++ src/main/java/com/jonefywang/blog/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.jonefywang.blog.controller;
 
 
+import com.jonefywang.blog.common.lang.Result;
+import com.jonefywang.blog.entity.User;
+import com.jonefywang.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 /**
  * <p>
@@ -17,4 +24,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/index")
+    public Object index(){
+        User user = userService.getById(1L);
+        return Result.success(200,"操作成功！", user);
+    }
 }
